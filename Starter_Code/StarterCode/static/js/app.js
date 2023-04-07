2bd4012a3121a9ab2a9b50719c7c8453d72f3a6f
deanjustin32 <119831680+deanjustin32@users.noreply.github.com>
2023-04-07 14:53:40 -0700

    updates

--- Starter_Code/StarterCode/static/js/app.js
+++ Starter_Code/StarterCode/static/js/app.js
@@ -1,4 +1,3 @@
-// Use the D3 library to read in samples.json from the URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
 function init() {
@@ -20,8 +19,7 @@ function init() {
     });
   }
   
-  
-  // Initialize the dashboard
+
   init();
   
   function optionChanged(newSample) {
@@ -30,7 +28,6 @@ function init() {
     
   }
   
-  // Demographic information 
   function buildMetadata(sample) {
     d3.json(url).then((data) => {
       var metadata = data.metadata;
@@ -47,7 +44,6 @@ function init() {
     });
   }
   
-  // Create a Horizontal Bar Chart
   function buildCharts(sample) {
     d3.json(url).then((data) => {
       console.log(data);
